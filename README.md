2c5b2f96e4b7a5120b6bdbb17fc38fc63077efda
Jack Frankland <30410009+FranklandJack@users.noreply.github.com>
2017-10-25 19:58:06 +0100

    added input directory and sample file, updated readme instructions and main method to calculate the probabilty of percolating over a number of trials

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Programs to test whether a grid can be percolated through.
 
 Initially this will only be for 2D grids. Once this is working and tested I will try it for 3D grids.
 
+To build; run make
+To run; run ./Percolate file.txt numberOfTrials
+where file.txt must be of the format: #rows
+									  #columns
+									  density 
+
+To generate documentation; run doxygen Doxyfile.
--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,72 @@
 #include <iostream>
 #include <chrono>
 #include <random>
+#include <fstream>
+#include <sstream>
 
 int main(int argc, char const *argv[])
 {
+    auto start = std::chrono::system_clock::now();
 
     unsigned int seed = static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count());
     std::default_random_engine generator(seed);
 
-    Grid2D myGrid(5,5,0.001,generator);
 
-    std::cout << myGrid << std::endl;
+    // The parameters for the grid will be read from the file which is provided as the first command line argument.
+    std::ifstream oscillatorParameters(argv[1]);
+    
+    // Check we have sucessfully opened the file. 
+    if(!oscillatorParameters)
+    {
+        std::cerr << argv[1] << " could not be opened for reading" <<std::endl;
+    }
+    
+    // First line in file should be the number of rows, the second the columns, the third the density.
+    int rows, columns;
+    double density; 
 
-    myGrid.printValues();
+    oscillatorParameters >> rows >> columns >> density;
+    
+    // The number of trials for this run of the program should be the second command line argument.
+    std::istringstream ss(argv[2]);
+
+    int trials;
+
+    if(!(ss >> trials))
+    {
+        std::cerr << "invalid trial number: " << argv[3] << std::endl;
+        exit(1);
+    }
+
+
+    // Value to hold the number of trial grids that sucessfully percolate. 
+    int percolations;
+
+
+    // Generate the given number of grids and if they sucessfully percolate we record it.
+
+    for(int iteratrion = 0; iteratrion < trials; ++iteratrion)
+    {
+
+    Grid2D myGrid(rows, columns, density, generator);
+
     while(myGrid.update())
-    std::cout << std::endl;
     
+    if(myGrid.test()) 
+    {
+        ++percolations;
+    }
+    }
 
-    myGrid.printValues();
 
-
-
-    int numberChanges = 1;
-
+    double probablity = static_cast<double>(percolations)/trials;
     
-    std::cout << myGrid.test() << std::endl;
+    std::cout << "The probablity of percolating for a " << rows << " x " << columns << " grid with density " 
+    << density << " is " << probablity << std::endl;
+
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Time take to execute (us):   " << elapsed.count() << std::endl << std::endl; 
     
 
     return 0;
